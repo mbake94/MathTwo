371c8fe3829b9028105b3cb556e3c0399763d738
mbake94 <mbake94@gmail.com>
2020-04-20 13:02:01 -0700

    first commit

---
+++ src/MathTwo.java
@@ -0,0 +1,11 @@
+public class MathTwo {
+    public static void main(String[]args){
+        int myNumber;
+        double myOtherNumber;
+        myNumber = 2;
+        myOtherNumber = 1.7938;
+
+        System.out.println("My number is: " + myNumber);
+        System.out.println("My other number is: " + myOtherNumber);
+    }
+}
